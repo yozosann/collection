d96b07b3de20c30bb98351c26d4fd371a24292ea
yangtianjie <yangtianjie@meituan.com>
2017-12-13 19:04:24 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// 求和
+export const sum = arr => arr.reduce((prev, cur) => prev + cur, 0)
+
+// 获取 locations.search 参数
+export const getQuery = (key = '') => {
+  const query = qs.parse(location.search.slice(1))
+  return key ? query[key] : query
+}
+
+// 过滤 json
+export const only = (data = {}, keys = []) => keys.reduce((prev, cur) => {
+  prev[cur] = data[cur]
+  return prev
+}, {})
+
+// 解析 url
+export const parseUrl = (url = '') => {
+  const a = document.createElement('a')
+  a.href = url
+  return only(a, ['protocol', 'host', 'pathname', 'port', 'search', 'hash'])
+}
